b11913ae19f8ea8b369cbde827ee493add1d6c19
OLgaPonomarenko <fblackberries@gmail.com>
2021-08-19 22:45:06 +0300

    pattern

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -35,6 +35,9 @@ class ContactForm extends Component {
             className={s.input}
             type="text"
             name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
             value={this.state.name}
             onChange={this.handleChange}
           />
@@ -46,6 +49,9 @@ class ContactForm extends Component {
             className={s.input}
             type="tel"
             name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            required
             value={this.state.number}
             onChange={this.handleChange}
           />
